d62e69db254083bb499e0d108f51f2e81a8ed54c
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-08-26 01:26:26 +0330

    Update

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer class="bg-gray-800 text-gray-100 p-4 text-center">
-    <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+    <p>&copy; 2024 Sepehr Rezaee. All rights reserved.</p>
 </footer>
